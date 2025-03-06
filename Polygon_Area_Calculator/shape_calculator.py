58e2d682c2c76bbd6fed6f345148f676aff91365
Jonathan Adams <3ls-it@pm.me>
2025-03-06 18:06:49 -0500

    Progress update.

--- Polygon_Area_Calculator/shape_calculator.py
+++ Polygon_Area_Calculator/shape_calculator.py
@@ -1,3 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/env python3
+
 class Rectangle:
 
     def __init__(self, width, height):
@@ -5,31 +7,22 @@ class Rectangle:
         self.height = height
   
     def __str__(self):
-        pass
+        return f'Rectangle(width={self.width}, height={self.height})'
 
-    def set_width(self):
-        pass
+    def set_width(self, w):
+        self.width = w
 
-    def set_height(self):
-        pass
+    def set_height(self, h):
+        self.height = h
 
     def get_area(self):
-        """
-        Returns area (width * height) 
-        """
-        pass
+        return self.width * self.height
 
     def get_perimeter(self):
-        """
-        Returns perimeter (2 * width + 2 * height) 
-        """
-        pass
+        return 2 * self.width + 2 * self.height
 
     def get_diagonal(self):
-        """
-        Returns diagonal ((width ** 2 + height ** 2) ** .5)
-        """
-        pass
+        return (self.width**2 + self.height**2)**.5
 
     def get_picture(self):
         """
@@ -44,26 +37,58 @@ class Rectangle:
         pass
 
 
-class Square():
-    pass
+class Square(Rectangle):
+
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.side = side
+        self.width = self.side
+        self.height = self.side
+
+    def __str__(self):
+        return f'Square(side={self.side})'
+
+    def set_side(self, s):
+        self.side = s
+        self.width = self.side
+        self.height = self.side
+
+    def set_width(self, w):
+        self.set_side(w)
+
+    def set_height(self, h):
+        self.set_side(h)
+
+## Tests
+r1 = Rectangle(4, 6)
+print(r1)
+print(f'Area: {r1.get_area()}')
+print(f'Perimeter: {r1.get_perimeter()}')
+print(f'Diagonal: {r1.get_diagonal()}')
+print()
+
+r1.set_width(4)
+r1.set_height(3)
+print(r1)
+print(f'Area: {r1.get_area()}')
+print(f'Perimeter: {r1.get_perimeter()}')
+print(f'Diagonal: {r1.get_diagonal()}')
+print()
+
+s1 = Square(5)
+print(s1)
+print(f'Area: {s1.get_area()}')
+print(f'Perimeter: {s1.get_perimeter()}')
+print(f'Diagonal: {s1.get_diagonal()}')
+print()
 
 
 
 """
-In this project you will use object oriented programming to create a Rectangle class and a Square class. The Square class should be a subclass of Rectangle, and inherit its methods and attributes.
-
 Rectangle class
+
 When a Rectangle object is created, it should be initialized with width and height attributes. The class should also contain the following methods:
 
-set_width
-
-set_height
-
-get_area: Returns area (width * height)
-
-get_perimeter: Returns perimeter (2 * width + 2 * height)
-
-get_diagonal: Returns diagonal ((width ** 2 + height ** 2) ** .5)
 
 get_picture: Returns a string that represents the shape using lines of '*'. The number of lines should be equal to the height and the number of '*' in each line should be equal to the width. There should be a new line (\n) at the end of each line. If the width or height is larger than 50, this should return the string: 'Too big for picture.'.
 
@@ -71,7 +96,9 @@ get_amount_inside: Takes another shape (square or rectangle) as an argument. Ret
 
 Additionally, if an instance of a Rectangle is represented as a string, it should look like: 'Rectangle(width=5, height=10)'.
 
+
 Square class
+
 The Square class should be a subclass of Rectangle. When a Square object is created, a single side length is passed in. The __init__ method should store the side length in both the width and height attributes from the Rectangle class.
 
 The Square class should be able to access the Rectangle class methods but should also contain a set_side method. If an instance of a Square is represented as a string, it should look like: 'Square(side=9)'.
