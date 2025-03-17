65f46c49d8419ec60828ebc5be3bf55da9b59955
Jonathan Adams <3ls-it@pm.me>
2025-03-17 10:51:21 -0400

    Adding internal fuctions

--- mean-variance-standard-deviation-calculator/mean_var_std.py
+++ mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -1,10 +1,25 @@
 #!/data/data/com.termux/files/usr/bin/env python3
-
 import numpy as np
 
-def calculate(list):
+
+
+def calculate(list: list) -> dict:
+
+    def _mean(n_list: list) -> float:
+        return sum(n_list) / len(n_list)
+    # End _mean() 
+
+    def _variance(n_list: list) -> float:
+        if len(n_list) == 0:
+            raise ValueError("List can not be empty.")
+        mu = _mean(n_list)
+        return sum((x - mu) ** 2 for x in n_list) / len(n_list)
+    # End _variance() 
+
+
 
 
 
 
     return calculations
+# End calculate() 
