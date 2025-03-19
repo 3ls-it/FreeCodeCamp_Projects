306f49f3e891841506e7e19f0fb13461bda7d7ea
Jonathan Adams <3ls-it@pm.me>
2025-03-18 22:12:17 -0400

    One more passes test.

--- demographic-data-analyzer/demographic_data_analyzer.py
+++ demographic-data-analyzer/demographic_data_analyzer.py
@@ -5,9 +5,10 @@ import pandas as pd
 def calculate_demographic_data(print_data=True):
     # Read data from file 
     df = pd.read_csv('adult.data.csv')
-    #print(df)
+    dfsize = len(df)
 
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    # How many of each race are represented in this dataset?  
+    # This should be a Pandas series with race names as the index labels. 
     races = df['race']
     race_count = races.value_counts()
     #print(race_count)
@@ -17,25 +18,31 @@ def calculate_demographic_data(print_data=True):
     #print(average_age_men)
 
     # What is the percentage of people who have a Bachelor's degree?  
-    percentage_bachelors = None
+    with_bachelors = (df['education'] == 'Bachelors').sum()
+    percentage_bachelors = round((with_bachelors / dfsize) * 100, 1)
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K? 
+    # What percentage of people with advanced education 
+    # (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K? 
+    bachelors_50k = ((df['education'] == 'Bachelors') & (df['salary'] == '>50K')).sum()
+    masters_50k = ((df['education'] == 'Masters') & (df['salary'] == '>50K')).sum()
+    doctorate_50k = ((df['education'] == 'Doctorate') & (df['salary'] == '>50K')).sum()
+    adv_sum = bachelors_50k + masters_50k + doctorate_50k
+    higher_education = round((adv_sum / dfsize) * 100, 1)
+
     # What percentage of people without advanced education make more than 50K?  
-
     # with and without `Bachelors`, `Masters`, or `Doctorate` 
-    higher_education = None
     lower_education = None
 
     # percentage with salary >50K  
-    higher_education_rich = None
+    higher_education_rich = higher_education
     lower_education_rich = None
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?  
     min_work_hours = None
 
-    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?  
+    # What percentage of the people who work the minimum number of hours 
+    # per week have a salary of >50K?  
     num_min_workers = None
-
     rich_percentage = None
 
     # What country has the highest percentage of people that earn >50K?  
